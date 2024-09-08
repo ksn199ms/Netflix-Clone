68c679f81107033c080ae5ec5208eb993b5c218f
ksn199ms <mubashirpannur123@gmail.com>
2024-09-08 11:19:27 +0530

    Sign In and Sign Up form design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+
+import {Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+
+      </Routes>
     </div>
   )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
+import logo from '../../assets/logo.png'
+
 const Login = () => {
+
+  const [signState,setSignState] = useState("Sign In")
+
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={logo} alt="" className='login-logo' />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+
+          {signState === "Sign Up" ? <input type="text" name="username" id="username" placeholder='Your Name' /> : <></> }
+          
+          <input type="email" name="email" id="email" placeholder='Email' />
+          <input type="text" name="password" id="password" placeholder='Password' />
+          <button>{signState}</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" name="remember" id="remember" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          { signState === "Sign In"  
+           ? <p>New to Netflix? <span onClick={() => setSignState("Sign Up")}>Sign Up</span></p> 
+           : <p>Already have an account? <span onClick={() => setSignState("Sign In")}>Sign In</span></p>
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
