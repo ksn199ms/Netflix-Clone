6ff4d0444eeb22b01820ede0989507c1c5b32308
ksn199ms <mubashirpannur123@gmail.com>
2024-09-08 21:07:22 +0530

    Firebase added and make it site responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,24 +1,47 @@
 import React, { useState } from 'react'
 import './Login.css'
+import {login, signup} from '../../firebase'
 
 import logo from '../../assets/logo.png'
+
+import netflix_spinner from '../../assets/netflix_spinner.gif'
 
 const Login = () => {
 
   const [signState,setSignState] = useState("Sign In")
 
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const [loading,setLoading] = useState(false)
+
+  const user_auth = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    if(signState === "Sign In"){
+      await login(email, password)
+    } else {
+      await signup(name, email, password)
+    }
+    setLoading(false)
+  }
+
   return (
+
+    loading ? <div className='login-spinner'><img src={netflix_spinner} alt="" /></div> :
+
     <div className='login'>
       <img src={logo} alt="" className='login-logo' />
       <div className="login-form">
         <h1>{signState}</h1>
         <form>
 
-          {signState === "Sign Up" ? <input type="text" name="username" id="username" placeholder='Your Name' /> : <></> }
+          {signState === "Sign Up" ? <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="username" id="username" placeholder='Your Name' /> : <></> }
           
-          <input type="email" name="email" id="email" placeholder='Email' />
-          <input type="text" name="password" id="password" placeholder='Password' />
-          <button>{signState}</button>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} name="email" id="email" placeholder='Email' />
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" id="password" placeholder='Password' />
+          <button onClick={user_auth} type='submit'>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" name="remember" id="remember" />
