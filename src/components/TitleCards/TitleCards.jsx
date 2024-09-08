bbcc63efe6116046644d93e4060bc086aebcde68
ksn199ms <mubashirpannur123@gmail.com>
2024-09-08 08:21:45 +0530

    Home page design completed

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './TitleCards.css'
 
+import cards_data from '../../assets/cards/Cards_data.js'
+
 const TitleCards = () => {
   return (
     <div className='titleCards'>TitleCards</div>
